a7acba41e2e06cc3863d3cd8282579e0875cefbb
Arthur <Debruit63@gmai.com>
2020-03-10 00:06:53 +0400

    fix: solve it

--- src/index.js
+++ src/index.js
@@ -1,12 +1,82 @@
 function eval() {
-    // Do not use eval!!!
-    return;
+  // Do not use eval!!!
+  return;
 }
+
+let whoFirst = {
+  "+": 1,
+  "-": 1,
+  "*": 2,
+  "/": 2
+};
 
 function expressionCalculator(expr) {
-    // write your solution here
-}
+  let arr = expr.split("");
+  let digit = [];
+  let oper = [];
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (!isNaN(arr[i])) {
+      digit.push(arr[i]);
+    }
+    if (isNaN(arr[i])) {
+      oper.push(arr[i]);
+      if (whoFirst[oper[i]] < whoFirst[oper[i - 1]]) {
+        if (oper[i] === "*") {
+          sum = digit[oper.length - 2] * digit[oper.length - 1];
+        }
+        if (oper[i] === "/") {
+          sum = digit[oper.length - 2] / digit[oper.length - 1];
+        }
+        digit
+          .pop()
+          .pop()
+          .push(sum);
+        console.log(digit);
+      }
+      if (whoFirst[oper[i]] === whoFirst[oper[i - 1]]) {
+        if (oper[i] === "*") {
+          sum = digit[oper.length - 2] * digit[oper.length - 1];
+        }
+        if (oper[i] === "/") {
+          sum = digit[oper.length - 2] / digit[oper.length - 1];
+        }
+        if (oper[i] === "+") {
+          sum = digit[oper.length - 2] + digit[oper.length - 1];
+        }
+        if (oper[i] === "-") {
+          sum = digit[oper.length - 2] - digit[oper.length - 1];
+          digit;
+        }
+        digit
+          .pop()
+          .pop()
+          .push(sum);
+        console.log(digit);
+      }
+    }
+  }
 
-module.exports = {
-    expressionCalculator
+  if (oper.length === 1 && oper[0] === "/") {
+    if (digit[1] === 0) {
+      throw Error("Делить на ноль нельзя!");
+    } else {
+      sum = digit[0] / digit[1];
+    }
+  }
+  if (oper.length === 1 && oper[0] === "*") {
+    sum = digit[0] * digit[1];
+  }
+  if (oper.length === 1 && oper[0] === "+") {
+    sum = digit[0] + digit[1];
+  }
+  if (oper.length === 1 && oper[0] === "-") {
+    sum = digit[0] - digit[1];
+  }
+  console.log(digit);
+  console.log(oper);
+  return sum;
 }
+module.exports = {
+  expressionCalculator
+};
